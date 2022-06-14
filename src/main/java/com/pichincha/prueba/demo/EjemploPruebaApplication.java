d96c734b324d87298ac8c1c1025bd6048c22f007
Janeth Molina <janeth_molina@hotmail.com>
2022-06-14 16:28:23 -0500

    Carga inicial del proyecto

---
+++ src/main/java/com/pichincha/prueba/demo/EjemploPruebaApplication.java
@@ -0,0 +1,13 @@
+package com.pichincha.prueba.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemploPruebaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemploPruebaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pichincha/prueba/demo/EjemploPruebaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pichincha.prueba.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemploPruebaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
